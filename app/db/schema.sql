181d84f3c8305f83a151ebeeff02f9865a41704f
Nherrisa Mae Celeste <nherrisa.celeste@gmail.com>
2013-02-22 17:12:38 +0800

    added photo values

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 22, 2013 at 05:50 AM
+-- Generation Time: Feb 22, 2013 at 09:05 AM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.4.3
 
@@ -45,9 +45,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `display_name`, `real_name`, `location`, `email`, `date_of_birth`, `bio`, `photo`) VALUES
-(1, 'Jane', 'Jane Doe', 'Sydney, Australia', 'jane@doe.gmail.com', '1980-05-28', 'A dynamic and hard-working waitress seeking a role in Catering or Customer service which will expand her skills and knowledge. Capable to work under pressure both as a genuine team member as well as on her own initiative. Really motivated and always ready to learn new things.', NULL),
-(2, 'Matt', 'Matt Mullenweg', 'Manila, Philippines', 'matt.mullenweg@gmail.com', '1984-10-18', 'Enthusiastic professional, responsible person and computer literate. Proven leadership abilities in working as a team, handling multiple tasks, great adaptability to any enterprise''s environment. Looking to obtain a challenging position offering growth in international organizations in Food and Beverage area with the philosophy of customers support and complete satisfaction.', NULL),
-(3, 'Jake', 'Jake Rocheleau', 'Carson City, Nevada, USA', 'jake.rocheleau@gmail.com', '1985-10-01', 'Self motivated when I succeed, persuasive, convincing, nice treatment, fluidness conversational, proactive, and excellent with open public relationship, expert in computational experience skills.', NULL);
+(1, 'Jane', 'Jane Doe', 'Sydney, Australia', 'jane@doe.gmail.com', '1980-05-28', 'A dynamic and hard-working waitress seeking a role in Catering or Customer service which will expand her skills and knowledge. Capable to work under pressure both as a genuine team member as well as on her own initiative. Really motivated and always ready to learn new things.', 'jane-doe.jpg'),
+(2, 'Matt', 'Matt Mullenweg', 'Manila, Philippines', 'matt.mullenweg@gmail.com', '1984-10-18', 'Enthusiastic professional, responsible person and computer literate. Proven leadership abilities in working as a team, handling multiple tasks, great adaptability to any enterprise''s environment. Looking to obtain a challenging position offering growth in international organizations in Food and Beverage area with the philosophy of customers support and complete satisfaction.', 'matt-mullenweg.jpg'),
+(3, 'Jake', 'Jake Rocheleau', 'Carson City, Nevada, USA', 'jake.rocheleau@gmail.com', '1985-10-01', 'Self motivated when I succeed, persuasive, convincing, nice treatment, fluidness conversational, proactive, and excellent with open public relationship, expert in computational experience skills.', 'jake-rocheleau.jpg');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
